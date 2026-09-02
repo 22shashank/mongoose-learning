0b17184edf9b3b25e8eb11eb790eb71a9e13f5b2
shashank <singhshashank15082@gmail.com>
2026-09-02 18:25:07 +0530

    changes in sub_todo.model

--- models/sub_todo.models.js
+++ models/sub_todo.models.js
@@ -2,7 +2,23 @@ import mongoode frmo "mongoose";
 
 const userSchema = new mongoose.schema({
     username: String 
-})
+    type:strinng,
+    required:true,
+    unique:true
+    lowercase:true
+}
+email:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true
+},
+password:{
+    type:String,
+    required:[true,"password is required"]
+}
+
+)
 
 export const user = mongoose.model("user",userSchema)
 
