d2937d22561ae4e820a8131de942021a48852f61
shashank <singhshashank15082@gmail.com>
2026-09-04 13:50:35 +0530

    update in catogory.js

---
+++ models/todos/ecommerce/catogory.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({},{timestamps:true}
+
+name:{
+
+    type:String,
+    required:true,
+},
+description:{
+    export const Category = mongoose.model("Category",categorySchema)
+ 
+    
+
+    export 
