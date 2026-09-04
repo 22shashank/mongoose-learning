1eb967ead898b9ffb5aa90e4af37d2dff32ee51b
shashank <singhshashank15082@gmail.com>
2026-09-04 16:43:22 +0530

    change in hospital.js

---
+++ models/todos/ecommerce/Hospital managment/hospital.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose"
+
+const hosptialSchema = new mongoose.Schema({},{timestamps:true}
+
+    export const medicalRecord = mongoose.model("medicalRecord",medicalRecordSchema);
